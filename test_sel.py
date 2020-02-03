4aaaf144e9fd7bebbdab0123beebbf0fdcd3a6ee
avkhimen <avkhimen@ualberta.ca>
2020-02-02 21:07:45 -0700

    Added sleep for x seconds

--- test_sel.py
+++ test_sel.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import time
 
 options = Options()
 options.headless = True
@@ -7,6 +8,7 @@ options.headless = True
 try:
 	driver = webdriver.Firefox(options=options)
 	driver.get('http://www.ubuntu.com/')
+	time.sleep(10)
 	driver.close()
 except Exception as e:
 	print(e)

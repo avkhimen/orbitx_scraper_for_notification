6ec37b16d37f130b50059ceb40967180e313cdb3
avkhimen <avkhimen@ualberta.ca>
2020-02-04 11:58:27 -0700

    Refactored with multiple nested try-catch statements to see program execution

--- test_sel.py
+++ test_sel.py
@@ -6,11 +6,22 @@ options = Options()
 options.headless = True
 
 try:
+	print('Trying to create the driver')
 	driver = webdriver.Firefox(options=options)
-	driver.get('http://www.ubuntu.com/')
-	time.sleep(10)
-	driver.close()
 except Exception as e:
 	print(e)
+else:
+	try:
+		print('Trying to access ubuntu.com')
+		driver.get('http://www.ubuntu.com/')
+	except Exception as e:
+		print(e)
+	else:
+		time.sleep(10)
+		try:
+			print('Trying to close the driver')
+			driver.close()
+		except Exception as e:
+			print(e)
 
 print('Task done')
